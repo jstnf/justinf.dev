718b9364aaa7e24cd4ff0bba1dfe1fc328d4902d
Justin <justinbfigueroa@gmail.com>
2021-12-01 18:06:01 -0800

    Push current conjugation work

--- js/pandiwa.js
+++ js/pandiwa.js
@@ -1,117 +1,3 @@
-/* The conjugations */
-var UmConjugation = {
-  doNeutral   : function() {
-    if (rootIndex === 0) {
-      neutralResult = 'um' + rootWord;
-    } else if (rootIndex === 1) {
-      neutralResult = rootWord.charAt(0) + 'um' + rootWord.substring(1);
-    } else {
-      neutralResult = 'invalid root';
-    }
-  },
-  doPast      : function() {
-    if (rootIndex === 0) {
-      pastResult = 'um' + rootWord;
-    } else if (rootIndex === 1) {
-      pastResult = rootWord.charAt(0) + 'um' + rootWord.substring(1);
-    } else {
-      pastResult = 'invalid root';
-    }
-  },
-  doPresent   : function() {
-    if (rootIndex === 0) {
-      presentResult = 'um' + rootWord.charAt(0) + rootWord;
-    } else if (rootIndex === 1) {
-      presentResult = rootWord.charAt(0) + 'um' + rootWord.charAt(1) + rootWord;
-    } else {
-      presentResult = 'invalid root';
-    }
-  },
-  doFuture    : function() {
-    if (rootIndex === 0) {
-      futureResult = rootWord.charAt(0) + rootWord;
-    } else if (rootIndex === 1) {
-      futureResult = rootWord.substring(0, 2) + rootWord;
-    } else {
-      futureResult = 'invalid root';
-    }
-  }
-};
-
-var MagConjugation = {
-  doNeutral   : function() {
-    if (rootIndex === 0) {
-      neutralResult = 'mag-' + rootWord;
-    } else if (rootIndex === 1) {
-      neutralResult = 'mag' + rootWord;
-    } else {
-      neutralResult = 'invalid root';
-    }
-  },
-  doPast      : function() {
-    if (rootIndex === 0) {
-      pastResult = 'nag-' + rootWord;
-    } else if (rootIndex === 1) {
-      pastResult = 'nag' + rootWord;
-    } else {
-      pastResult = 'invalid root';
-    }
-  },
-  doPresent   : function() {
-    if (rootIndex === 0) {
-      presentResult = 'nag-' + rootWord.charAt(0) + rootWord;
-    } else if (rootIndex === 1) {
-      presentResult = 'nag' + rootWord.substring(0, 2) + rootWord;
-    } else {
-      presentResult = 'invalid root';
-    }
-  },
-  doFuture    : function() {
-    if (rootIndex === 0) {
-      futureResult = 'mag-' + rootWord.charAt(0) + rootWord;
-    } else if (rootIndex === 1) {
-      futureResult = 'mag' + rootWord.substring(0, 2) + rootWord;
-    } else {
-      futureResult = 'invalid root';
-    }
-  }
-};
-
-var MaConjugation = {
-  doNeutral   : function() {
-    if (rootIndex === 0 || rootIndex == 1) {
-      neutralResult = 'ma' + rootWord;
-    } else {
-      neutralResult = 'invalid root';
-    }
-  },
-  doPast      : function() {
-    if (rootIndex === 0 || rootIndex === 1) {
-      pastResult = 'na' + rootWord;
-    } else {
-      pastResult = 'invalid root';
-    }
-  },
-  doPresent   : function() {
-    if (rootIndex === 0) {
-      presentResult = 'na' + rootWord.charAt(0) + rootWord;
-    } else if (rootIndex === 1) {
-      presentResult = 'na' + rootWord.substring(0, 2) + rootWord;
-    } else {
-      presentResult = 'invalid root';
-    }
-  },
-  doFuture    : function() {
-    if (rootIndex === 0) {
-      futureResult = 'ma' + rootWord.charAt(0) + rootWord;
-    } else if (rootIndex === 1) {
-      futureResult = 'ma' + rootWord.substring(0, 2) + rootWord;
-    } else {
-      futureResult = 'invalid root';
-    }
-  }
-};
-
 let rootWord = '';
 let rootIndex = -1;
 
@@ -135,6 +21,18 @@ function setMode(event) {
       break;
     case 'ma':
       mode = MaConjugation;
+      break;
+    case 'mang':
+      break;
+    case 'man':
+      break;
+    case 'mam':
+      break;
+    case 'i':
+      break;
+    case 'inhin':
+      break;
+    case 'anhan':
       break;
   }
   doConjugationAndUpdate();
