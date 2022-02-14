bcdc66c7737d796989f9660b197cd2906c2e14fd
Justin <justinbfigueroa@gmail.com>
2022-02-14 14:31:47 -0800

    Current progress

--- src/components/RandomExperience.js
+++ src/components/RandomExperience.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Card from './Card';
 
-class RandomExperience extends React.Component {
-  render() {
-    return (
-      <div className="random-experience-container" id="random-experience">
-        <h1>Take a look!</h1>
-        <p>This is a random sample of my projects and experience.</p>
+export const RandomExperience = () => {
+  const [enabled, setEnabled] = React.useState(true);
 
-        <p className="random-button" onClick={console.log("test")}>Randomize</p>
-      </div>
-    );
+  const onRandomizePress = (e) => {
+    setEnabled(!enabled);
+    console.log(enabled);
   }
+
+  return (
+    <div className="random-experience-container" id="random-experience">
+      <h1 style={enabled ? {display: "inline"} : {display: "none"}}>Take a look!</h1>
+      <p>This is a random sample of my projects and experience.</p>
+
+      <button className="random-button" onClick={onRandomizePress}>Randomize</button>
+    </div>
+  );
 }
 
 export default RandomExperience;
