cc905e14b9978178454c435d855c4c94ced5e93d
jstnf <justinbfigueroa@gmail.com>
2022-02-14 14:13:18 -0800

    Commit current progress (Cards)

--- src/components/RandomExperience.js
+++ src/components/RandomExperience.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import Card from './Card';
+import data from "../assets/randomexperience.json";
+import "../css/cards.css";
 
-class RandomExperience extends React.Component {
-  render() {
-    return (
-      <div className="random-experience-container" id="random-experience">
-        <h1>Take a look!</h1>
-        <p>This is a random sample of my projects and experience.</p>
+export const RandomExperience = () => {
+  const [postIndices, setPostIndices] = React.useState([0, 1, 2, 3, 4]);
 
-        <p className="random-button" onClick={console.log("test")}>Randomize</p>
-      </div>
-    );
+  const randomizePostIndices = (e) => {
+    let newPostIndices = [];
+    for (let i = 0; i < 5; i++) {
+      let trial = Math.floor(Math.random() * data.posts.length);
+      while (newPostIndices.includes(trial)) {
+        trial = Math.floor(Math.random() * data.posts.length);
+      }
+      newPostIndices.push(trial);
+    }
+    setPostIndices(newPostIndices);
   }
+
+  return (
+    <div className="random-experience-container" id="random-experience">
+      <h1 className="random-experience-title">Take a look!</h1>
+      <p className="random-experience-subtitle">This is a random sample of my projects and experience.</p>
+
+      <div className="cards">
+        {postIndices.map((num) => {
+          return (
+            <div className="card">
+              <h2>{data.posts[num].title}</h2>
+              <p>{data.posts[num].content}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <button className="random-button" onClick={randomizePostIndices}>Randomize</button>
+    </div>
+  );
 }
 
 export default RandomExperience;
