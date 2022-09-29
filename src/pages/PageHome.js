fd1f2895da23789f5020c14acc747f0d08c0a069
Justin <justin@justinf.dev>
2022-09-29 16:30:06 -0700

    Remove unused experience components

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import Header from '../components/Header';
 import NavbarComponent from '../components/NavbarComponent';
-import RandomExperience from '../components/RandomExperience';
 import ProjectsComponent from '../components/ProjectsComponent';
-import ExperienceComponent from '../components/ExperienceComponent';
 import Footer from '../components/Footer';
 
 class PageHome extends React.Component {
@@ -13,9 +11,7 @@ class PageHome extends React.Component {
       <>
         <Header />
         <NavbarComponent />
-        <RandomExperience />
         <ProjectsComponent />
-        <ExperienceComponent />
         <Footer />
       </>
     );
