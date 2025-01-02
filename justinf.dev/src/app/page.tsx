6434af0d2e85d7cd9042ace2c97bab5cd46f7cd7
jstnf <justin@justinf.dev>
2025-01-01 23:42:24 -0800

    refactor: MetroSignCarousel component

--- justinf.dev/src/app/page.tsx
+++ justinf.dev/src/app/page.tsx
@@ -1,43 +1,12 @@
 "use client";
 
-import { MetroSign, MetroSignProps } from "@/components/MetroSign";
-import FloatingHeaderText from "@/components/FloatingHeaderText";
 import React from "react";
-import { animated, useSpring } from "@react-spring/web";
-import { Link } from "next-view-transitions";
+import FloatingHeaderText from "@/components/FloatingHeaderText";
+import MetroSignCarousel from "@/components/MetroSignCarousel";
 import MetroStandaloneEmblem from "@/components/MetroStandaloneEmblem";
+import type {MetroSignProps} from "@/components/MetroSign";
 
 export default function Home() {
-  const [highlighted, setHighlighted] = React.useState<number | null>(null);
-  React.useEffect(() => {
-    const handleScroll = () => {
-      setHighlighted(null);
-    };
-
-    const container = document.querySelector('.hide-scrollbar');
-    container?.addEventListener("scroll", handleScroll);
-
-    return () => {
-      container?.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const [isOverflowVisible, setIsOverflowVisible] = React.useState<boolean>(true);
-  const trainSlideInAnimation = useSpring({
-    from: {
-      transform: "translateX(-150%)",
-      opacity: 0
-    },
-    to: {
-      transform: "translateX(0)",
-      opacity: 1
-    },
-    config: {duration: 1000, easing: (t: number) => t * t * (3 - 2 * t)}, // Ease-in-out effect
-    onRest: () => {
-      setIsOverflowVisible(false);
-    },
-  });
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-20 pb-16 gap-16 font-frutiger">
       <header className="row-start-1 flex gap-6 items-center justify-center">
@@ -48,28 +17,7 @@ export default function Home() {
         <div className="w-full">
           <FloatingHeaderText title="Where to go?" subtitle="(click one)"/>
         </div>
-        <animated.div
-          className="flex gap-8 w-full max-w-full px-8 py-4 sm:px-16 hide-scrollbar"
-          style={{
-            ...trainSlideInAnimation,
-            overflowX: isOverflowVisible ? "visible" : "auto",
-          }}
-        >
-          {signs.map((sign, index) => (
-            <animated.div
-              key={index}
-              style={{
-                opacity: highlighted === null || highlighted === index ? 1 : 0.6,
-                transform: highlighted === null ? "scale(1)" : (highlighted === index ? "scale(1.1)" : "scale(0.9)"),
-                transition: "opacity 0.3s, transform 0.3s",
-              }}
-              onMouseEnter={() => setHighlighted(index)}
-              onMouseLeave={() => setHighlighted(null)}
-            >
-              <Link href={sign.href}><MetroSign signProps={sign}/></Link>
-            </animated.div>
-          ))}
-        </animated.div>
+        <MetroSignCarousel signs={signs}/>
       </main>
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
---
+++ justinf.dev/src/components/MetroSignCarousel.tsx
@@ -0,0 +1,65 @@
+import {animated, useSpring} from "@react-spring/web";
+import {Link} from "next-view-transitions";
+import {MetroSign, MetroSignProps} from "@/components/MetroSign";
+import React from "react";
+
+export default function MetroSignCarousel({
+  signs
+}: Readonly<{
+  signs: (MetroSignProps & { href: string })[]
+}>) {
+  const [highlighted, setHighlighted] = React.useState<number | null>(null);
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setHighlighted(null);
+    };
+
+    const container = document.querySelector('.hide-scrollbar');
+    container?.addEventListener("scroll", handleScroll);
+
+    return () => {
+      container?.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const [isOverflowVisible, setIsOverflowVisible] = React.useState<boolean>(true);
+  const trainSlideInAnimation = useSpring({
+    from: {
+      transform: "translateX(-150%)",
+      opacity: 0
+    },
+    to: {
+      transform: "translateX(0)",
+      opacity: 1
+    },
+    config: {duration: 1000, easing: (t: number) => t * t * (3 - 2 * t)}, // Ease-in-out effect
+    onRest: () => {
+      setIsOverflowVisible(false);
+    },
+  });
+
+  return (
+    <animated.div
+      className="flex gap-8 w-full max-w-full px-8 py-4 sm:px-16 hide-scrollbar"
+      style={{
+        ...trainSlideInAnimation,
+        overflowX: isOverflowVisible ? "visible" : "auto",
+      }}
+    >
+      {signs.map((sign, index) => (
+        <animated.div
+          key={index}
+          style={{
+            opacity: highlighted === null || highlighted === index ? 1 : 0.6,
+            transform: highlighted === null ? "scale(1)" : (highlighted === index ? "scale(1.1)" : "scale(0.9)"),
+            transition: "opacity 0.3s, transform 0.3s",
+          }}
+          onMouseEnter={() => setHighlighted(index)}
+          onMouseLeave={() => setHighlighted(null)}
+        >
+          <Link href={sign.href}><MetroSign signProps={sign}/></Link>
+        </animated.div>
+      ))}
+    </animated.div>
+  )
+}
